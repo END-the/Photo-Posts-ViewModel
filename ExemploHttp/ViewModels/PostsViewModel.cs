778d844ea9064ca4ce62913a4dfdd3b8eff06deb
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-08-06 12:50:58 -0300

    feat: added MVVM struct

---
+++ ExemploHttp/ViewModels/PostsViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using ExemploHttp.Models;
+using ExemploHttp.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ExemploHttp.ViewModels
+{
+    public partial class PostsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Post> posts;
+
+        public ICommand getPostsCommand;
+
+        public PostsViewModel()
+        {
+            getPostsCommand = new Command(getPosts);
+        }
+
+        public async void getPosts()
+        {
+           RestService restService = new RestService();
+           Posts = await restService.getPostAsync();
+        }
+    }
+}
